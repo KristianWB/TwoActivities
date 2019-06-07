e658a5c866a33b786212309e3c2cbe6e48535c69
KristianWB <42993999+KristianWB@users.noreply.github.com>
2019-06-07 11:06:35 +0200

    - Added extra message intent to launchSecondActivity() method

--- app/src/main/java/com/example/twoactivities/MainActivity.java
+++ app/src/main/java/com/example/twoactivities/MainActivity.java
@@ -5,22 +5,31 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     // Setup logging of button activation for main button
     private static final String LOG_TAG = MainActivity.class.getSimpleName();
+    // KEY for Intent EXTRA and a variable to hold the text
+    public static final String EXTRA_MESSAGE = "com.example.android.twoactivities.extra.MESSAGE";
+    private EditText mMessageEditText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mMessageEditText = (EditText)findViewById(R.id.editText_main);
     }
 
     // Setting up method for onClick on main button, intent and message
     public void launchSecondActivity(View view) {
         Log.d(LOG_TAG, "button clicked");
         Intent intent = new Intent(this, SecondActivity.class);
+        String message = mMessageEditText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
     }
 }
